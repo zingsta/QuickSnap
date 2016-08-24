3803bc83738d722a4dd95f5be0e00c4837538e6f
FulLachy <101106666@student.swin.edu.au>
2016-08-24 11:18:26 +1000

    add new font and implemented in game

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -40,9 +40,10 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
+				SwinGame.LoadFontNamed("GameFont", "Chunkfive.otf", 24);
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, "GameFont", 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont", 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
